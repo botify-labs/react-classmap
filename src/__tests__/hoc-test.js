7888ae97ab8d6f6995bfb3fb0ba2d82391054fe4
Morhaus <alexandre.kirszenberg@gmail.com>
2015-10-29 15:32:59 +0100

    HOC preserves non-react statics

--- src/__tests__/hoc-test.js
+++ src/__tests__/hoc-test.js
@@ -38,4 +38,19 @@ describe('classMap', () => {
 
   });
 
+  it('preserves non react statics', () => {
+
+    @classMap({})
+    class Test extends React.Component {
+      static foo = 'bar';
+
+      render() {
+        return <div className="child" />;
+      }
+    }
+
+    expect(Test.foo).toBe('bar');
+
+  });
+
 });
